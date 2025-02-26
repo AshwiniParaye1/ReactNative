322615575701d5a73a8cfd9a72e8f5b5733c7637
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-02-26 16:15:38 +0530

    styling

--- firstapp/App.tsx
+++ firstapp/App.tsx
@@ -1,102 +1,53 @@
-import {
-  Text,
-  SafeAreaView,
-  Image,
-  Button,
-  TouchableOpacity,
-  Pressable,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
 const App = () => {
-  const theme = useColorScheme();
-  const isDarkMode = theme === 'dark';
-
-  const backgroundColor = isDarkMode ? 'black' : 'white';
-  const textColor = isDarkMode ? 'white' : 'black';
-
-  const style = {
-    container: {
-      marginTop: 50,
-      marginLeft: 20,
-      marginRight: 20,
-    },
-  };
-
   return (
-    <SafeAreaView style={[style.container, {backgroundColor: backgroundColor}]}>
-      <Text style={[styles.headingText, {color: textColor}]}>
-        Hello, Native People
-      </Text>
-
-      <Image
-        style={styles.imageContainer}
-        source={{
-          uri: 'https://plus.unsplash.com/premium_photo-1676009547155-32d75ba9d089?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        }}
-      />
-
-      <Button title="Normal Button" />
-
-      <TouchableOpacity style={styles.touchableBtn}>
-        <Text style={[styles.touchableText, {color: textColor}]}>
-          Touchable Opacity
-        </Text>
-      </TouchableOpacity>
-
-      <Pressable style={styles.pressableBtn}>
-        <Text style={[styles.pressableText, {color: textColor}]}>
-          Pressable Button
-        </Text>
-      </Pressable>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={[styles.box3, {backgroundColor: 'blue'}]} />
+    </View>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  imageContainer: {
-    width: 200,
-    height: 200,
-    marginBottom: 20,
-    alignSelf: 'center',
+  container: {
+    paddingTop: 50,
+    flex: 1,
+    backgroundColor: 'black',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   },
-  button: {
-    borderRadius: 20,
+  box1: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'red',
+    // alignSelf: 'flex-start',
   },
-
-  headingText: {
-    marginBottom: 20,
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  box2: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'yellow',
   },
-
-  pressableBtn: {
-    backgroundColor: 'purple',
-    marginTop: 20,
-    padding: 10,
-    borderRadius: 6,
-  },
-
-  pressableText: {
-    fontSize: 17,
-    fontWeight: 'medium',
-    textAlign: 'center',
-  },
-
-  touchableBtn: {
+  box3: {
+    width: 80,
+    height: 80,
     backgroundColor: 'green',
-    marginTop: 20,
-    padding: 10,
-    borderRadius: 6,
-  },
-  touchableText: {
-    fontSize: 17,
-    fontWeight: 'medium',
-    textAlign: 'center',
   },
 });
