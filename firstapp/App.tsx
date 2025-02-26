39b48895161e65838d8aa81a7c1dae5c33b54cca
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-02-26 16:32:28 +0530

    scrollview

--- firstapp/App.tsx
+++ firstapp/App.tsx
@@ -1,12 +1,31 @@
-import {StyleSheet, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {ScrollView, StyleSheet, View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView
+      contentContainerStyle={{gap: 10}}
+      horizontal
+      style={styles.container}>
       <View style={styles.box1} />
       <View style={styles.box2} />
       <View style={styles.box3} />
-    </View>
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
+    </ScrollView>
   );
 };
 
@@ -14,25 +33,27 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
     flex: 1,
     backgroundColor: 'black',
-    flexDirection: 'row',
+    paddingTop: 50,
   },
   box1: {
     width: 80,
     height: 80,
     backgroundColor: 'red',
+    borderRadius: 50,
   },
   box2: {
     width: 80,
     height: 80,
     backgroundColor: 'yellow',
     flexGrow: 1,
+    borderRadius: 50,
   },
   box3: {
     width: 80,
     height: 80,
     backgroundColor: 'green',
+    borderRadius: 50,
   },
 });
